deb848cfbd34d60daf0d2cf2d8e49609e797cf3f
Joscelyn <JoscelynRose@gmail.com>
2019-06-18 16:35:29 -0400

    added event listeners for nav and images and p tags

--- js/index.js
+++ js/index.js
@@ -1,1 +1,49 @@
-// Your code goes here
+// // Your code goes here
+
+/* target elements */
+const nav = document.querySelectorAll('.nav-link')
+const images = document.querySelectorAll('img');
+const p = document.querySelectorAll('p');
+
+nav.forEach((navItem) => {
+    navItem.addEventListener('click', function() {
+        if (navItem.style.background === 'red') {
+            navItem.style.background = 'blue';
+        } else {
+            navItem.style.background = 'red';
+        }
+    });
+})
+
+
+
+
+images.forEach((img) => {
+    img.addEventListener('mouseover', function() {
+        if (img.style.boxShadow != '0 4px 8px 0') {
+            img.style.boxShadow = '0 4px 8px 0';
+        } 
+    })
+    img.addEventListener('mouseout', function() {
+        img.style.boxShadow = 'none'
+    })
+})
+
+p.forEach((text) => {
+    text.addEventListener('click', function() {
+        text.style.fontSize = '16pt';
+    })
+})
+
+
+
+// * [ ] `mouseover`
+// * [ ] `keydown`
+// * [ ] `wheel`
+// * [ ] `drag / drop`
+// * [ ] `load`
+// * [ ] `focus`
+// * [ ] `resize`
+// * [ ] `scroll`
+// * [ ] `select`
+// * [ ] `dblclick`
